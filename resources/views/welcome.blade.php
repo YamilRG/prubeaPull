11a4d51de30db56e89a94c2e3ac4ade1f0aabc84
YamilRG <eduardo_1600@hotmail.com>
2021-07-14 17:16:29 -0500

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelDefault3
+                    Larvel4
                 </div>
 
                 <div class="links">
