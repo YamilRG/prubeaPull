c796c753554097b0b10b9964f5b526893eb8d8d4
YamilRG <eduardo_1600@hotmail.com>
2021-07-14 16:14:52 -0500

    elpaco

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelo2
+                    elpaco
                 </div>
 
                 <div class="links">
