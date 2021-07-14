08d4bf3cb0a11adbd69ecc5abb13ca9140108869
YamilRG <eduardo_1600@hotmail.com>
2021-07-14 16:06:20 -0500

    pruebacliente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelo
                 </div>
 
                 <div class="links">
