cc53bd064fa44bdde55cf6f9782dab4788220bf8
YamilRG <eduardo_1600@hotmail.com>
2021-07-14 16:50:05 -0500

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelDefault
+                    LaravelDefault2
                 </div>
 
                 <div class="links">
