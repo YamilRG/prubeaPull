258054f2fafac86c15203f0da1bb6826ec6cc4b8
YamilRG <eduardo_1600@hotmail.com>
2021-07-15 13:56:42 -0500

    Cambio de Controlador

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
  	public function mostrar(){
-        return json_encode(DB::select('select * from warro;'));
+        return json_encode(DB::select('select * from warros;'));
         }
 
 	public function prueba(){
