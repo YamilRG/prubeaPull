9da5072e6a71665603ca4a97d2d79d2e8a674409
YamilRG <eduardo_1600@hotmail.com>
2021-07-15 14:24:37 -0500

    cambio

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/mostrar', [\App\Http\Controllers\Controller::class, 'mostrar']);
+Route::get('/mostrara', [\App\Http\Controllers\Controller::class, 'mostrar']);
 
 Route::post('/registro', function(Request $request){
 	$warro = new warros();
